c4789cb0d4416e83a18541da15754679a2df8875
victoriacr <victoriacalvrodri@gmail.com>
2022-05-04 16:56:56 +0200

    Primer commit

---
+++ src/Code11.java
@@ -0,0 +1,32 @@
+// Consultar taula https://en.wikipedia.org/wiki/Barcode#Linear_barcodes
+// Code11: https://en.wikipedia.org/wiki/Code_11
+
+// Generadors de codis:
+//     https://barcode.tec-it.com/en/Code11
+//     https://www.free-barcode-generator.net/code-11/
+//     https://products.aspose.app/barcode/generate
+
+public class Code11 {
+
+    // Codifica un String amb Code11
+    static String encode(String s) {
+        return "";
+    }
+
+    // Decodifica amb Code11
+    static String decode(String s) {
+        return "";
+    }
+
+    // Decodifica una imatge. La imatge ha d'estar en format "ppm"
+    public static String decodeImage(String str) {
+        return "";
+    }
+
+    // Genera imatge a partir de codi de barres
+    // Alçada: 100px
+    // Marges: vertical 4px, horizontal 8px
+    public static String generateImage(String s) {
+        return "";
+    }
+}
---
+++ src/Code93.java
@@ -0,0 +1,27 @@
+// https://en.wikipedia.org/wiki/Code_93
+
+public class Code93 {
+
+    // Codifica emprant Code93
+    static String encode(String str) {
+        return "";
+    }
+
+    // Decodifica emprant Code93
+    static String decode(String str) {
+        return "";
+    }
+
+    // Decodifica una imatge. La imatge ha d'estar en format "ppm"
+    public static String decodeImage(String str) {
+        return "";
+    }
+
+    // Genera imatge a partir de barcode code93
+    // Unitat barra mínima: 3 pixels
+    // Alçada: 100px
+    // Marges: vertical: 5px, horizontal: 15px
+    public static String generateImage(String s) {
+        return "";
+    }
+}
