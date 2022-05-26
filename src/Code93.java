f61314a725e1680bcf670197ab675913d0fb762a
victoriacr <victoriacalvrodri@gmail.com>
2022-05-26 16:56:27 +0200

    Empezando code93 26/5.

--- src/Code93.java
+++ src/Code93.java
@@ -1,10 +1,77 @@
 // https://en.wikipedia.org/wiki/Code_93
 
 public class Code93 {
+    static String caracters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ-. $/+%????*";
+
+    static String[] grossors = new String[]{
+            "131112",
+            "111213",
+            "111312",
+            "111411",
+            "121113",
+            "121212",
+            "121311",
+            "111114",
+            "131211",
+            "141111",
+            "211113",
+            "211212",
+            "211311",
+            "221112",
+            "221211",
+            "231111",
+            "112113",
+            "112212",
+            "112311",
+            "122112",
+            "132111",
+            "111123",
+            "111222",
+            "111321",
+            "121122",
+            "131121",
+            "212112",
+            "212211",
+            "211122",
+            "211221",
+            "221121",
+            "222111",
+            "112122",
+            "112221",
+            "122121",
+            "123111",
+            "121131",
+            "311112",
+            "311211",
+            "321111",
+            "112131",
+            "113121",
+            "211131",
+            "121221",
+            "312111",
+            "311121",
+            "122211",
+            "111141"
+    };
 
     // Codifica emprant Code93
     static String encode(String str) {
-        return "";
+        String resultat = "";
+        for (int i = 0; i < str.length(); i++) {
+            char caracter = str.charAt(i);
+            cercarIndexCaracter(caracter, caracters);
+            System.out.println(cercarIndexCaracter(caracter, caracters));
+        }
+        return resultat;
+    }
+
+    private static int cercarIndexCaracter(char caracter, String string) {
+        for (int i = 0; i < string.length(); i++) {
+            if (caracter == string.charAt(i)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     // Decodifica emprant Code93
